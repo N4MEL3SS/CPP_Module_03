7273f88f3713cf0bde1d7166ba7bd16ccdcd4894
celadia <celadia@student.21-school.ru>
2022-09-14 16:05:30 +0300

    Add Gazebo!

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -2,20 +2,20 @@
 
 ClapTrap::ClapTrap()
 {
-	this->_name = "CLAPTRAP!!!";
+	_name = "CLAPTRAP!!!";
 	_hitPoints = 10;
 	_energyPoints = 10;
 	_attackDamage = 0;
-	std::cout << this->_name << " beatbox time, check it!" << std::endl;
+	std::cout << _name << " beatbox time, check it!" << std::endl;
 }
 
 ClapTrap::ClapTrap(std::string name)
 {
-	this->_name = name;
+	_name = name;
 	_hitPoints = 10;
 	_energyPoints = 10;
 	_attackDamage = 0;
-	std::cout << this->_name << " beatbox time, check it!" << std::endl;
+	std::cout << _name << " beatbox time, check it!" << std::endl;
 }
 
 ClapTrap::~ClapTrap()
@@ -27,28 +27,53 @@ void ClapTrap::attack(const std::string& target)
 {
 	if (_energyPoints > 0)
 	{
+		_energyPoints--;
+		std::cout << _name << " attacks " << target << std::endl;
 
+		if (target == "Gazebo")
+			std::cout << target << " ignored you." << std::endl;
+		else
+			std::cout << target << " get " << _attackDamage << " points of damage!" << std::endl;
+		std::cout << _name << " energy: " << _energyPoints << std::endl;
+	}
+	else
+	{
+		std::cout << _name << ": Not enough energy!" << std::endl;
 	}
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-
+	if (_hitPoints <= amount)
+	{
+		_hitPoints = 0;
+		std::cout << _name << " was destroyed!" << std::endl;
+	}
+	else
+	{
+		_hitPoints -= amount;
+		std::cout << _name << " has taken " << amount << " point damage." << std::endl;
+		std::cout << _name << " hit points: " << _hitPoints << std::endl;
+	}
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	if (_energyPoints < 1)
-	{
-		std::cout << _name << " You don't have enough energy" << std::endl;
-	}
-	else
+	if (_hitPoints < 1)
+		return;
+
+	if (_energyPoints > 0)
 	{
 		_energyPoints--;
 		_hitPoints += amount;
 
-		std::cout << _name << " - Health replenished by " << amount << " points" << std::endl;
-		std::cout << _name << " - Health " << _hitPoints << std::endl;
+		std::cout << _name << " - Health replenished by " << amount << " points." << std::endl;
+		std::cout << _name << " - Health: " << _hitPoints << std::endl;
+		std::cout << _name << " energy: " << _energyPoints << std::endl;
+	}
+	else
+	{
+		std::cout << _name << ": Not enough energy!" << std::endl;
 	}
 }
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,7 +1,27 @@
 #include "ClapTrap.hpp"
+#include <iostream>
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	ClapTrap Trap;
+	ClapTrap Gazebo("Gazebo");
+
+	std::cout << "________________________________" << std::endl;
+	std::cout << std::endl;
+
+	for(int i = 0; i < 12; i++)
+		Trap.attack("Gazebo");
+	std::cout << std::endl;
+
+	Gazebo.attack("CLAPTRAP!!!");
+	Trap.takeDamage(10);
+	std::cout << std::endl;
+
+	Trap.beRepaired(10);
+	Gazebo.beRepaired(10);
+
+	std::cout << std::endl;
+	std::cout << "________________________________" << std::endl;
+
 	return 0;
 }
